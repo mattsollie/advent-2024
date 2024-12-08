d03c126a7bf9ba30aaf677ef293110e70c39e7c4
Matt Sollie <mattsollie@gmail.com>
2024-12-08 14:27:49 -0700

    fixed dumb mistake in day 6

--- 6/app.py
+++ 6/app.py
@@ -4,7 +4,8 @@ Q2 = 0
 
 def get_data():
     data = []
-    with open('sample.txt') as f:
+    map = []
+    with open('/home/matt/repos/advent-2024/6/data.txt') as f:
         for line in f:
             data.append(line.strip())
     data = data[::-1]       
@@ -13,13 +14,16 @@ def get_data():
     start = []
     for y, row in enumerate(data):
         horizontal.append([])
+        map_tmp = []
         for x, char in enumerate(row):
+            map_tmp.append(char)
             if char == '#':
                 horizontal[-1].append(x)
                 vertical[x].append(y)
             elif char == "^":
                 start = [x,y]
-    return(start, horizontal, vertical)
+        map.append(map_tmp)
+    return(start, horizontal, vertical, map)
 
 def turn(direction):
     directions = ["N","E","S","W"]
@@ -32,17 +36,14 @@ def turn(direction):
 
 def search_path(pos, path, max, inverse=False):
     blocked = False
-    #print("   Test Path {}".format(path))
     if not inverse:
         for block in path:
-            #print("testing block {}".format(block) )
             if block >= pos:
                 new_pos = block - 1
                 blocked = True
                 break
     else: 
         for block in reversed(path):
-            #print("testing inverse block {}: ".format(block))
             if block <= pos:
                 new_pos = block + 1
                 blocked = True
@@ -54,15 +55,33 @@ def search_path(pos, path, max, inverse=False):
             return [-1, pos + 1, 0]
     return(new_pos, abs(new_pos-pos), block)
 
+def draw_map(map, visited, test_pos=[], loop=[]):
+    map = copy.deepcopy(map)
+    for i,c in enumerate(visited):
+        #print(c)
+        coord = c.split(':')
+        map[int(coord[1])][int(coord[0])] = "X"
+    if len(loop) > 0:
+        for c in loop:
+            coord = c.split(':')
+            map[int(coord[1])][int(coord[0])] = "@"
+    if len(test_pos) > 0:
+            map[int(test_pos[1])][int(test_pos[0])] = "="
+    start = visited[0].split(':')
+    end = visited[-1].split(':')
+    print("start {} and end {}".format(start,end))
+    map[int(start[1])][int(start[0])] = "^"
+    map[int(end[1])][int(end[0])] = "0"
+
+    for i,row in enumerate(map[::-1]):
+        print(''.join(row) + "|" + str(129-i))
+
 def check_unique(locations, all_visited):
     i = 0
     for location in locations:
         loc = "{}:{}".format(location[0], location[1])
         if loc not in (all_visited):
             all_visited.append(loc)
-        else:
-            #print("already been to {}".format(loc))
-            next
     return(all_visited)
 
 def move(start, direction, horizontal, vertical, all_visited, test=True):
@@ -87,14 +106,14 @@ def move(start, direction, horizontal, vertical, all_visited, test=True):
         new_pos, steps, next_block = search_path(start[1], vertical[start[0]], max_height, True)
         pos = [start[0], new_pos]
         if new_pos >= 0:
-            visited = [[start[0], i] for i in range(new_pos-1, start[1])]
+            visited = [[start[0], i] for i in range(new_pos, start[1])]
         else:
             visited = [[start[0], i] for i in range(0, start[1])]
     elif direction == "W":
         new_pos, steps, next_block = search_path(start[0], horizontal[start[1]], max_width, True)
         pos = [new_pos, start[1]]
         if new_pos >= 0:
-            visited = [[i, start[1]] for i in range(new_pos-1, start[0])]
+            visited = [[i, start[1]] for i in range(new_pos, start[0])]
         else:
             visited = [[i, start[1]] for i in range(0, start[0])]
         
@@ -103,69 +122,78 @@ def move(start, direction, horizontal, vertical, all_visited, test=True):
     if test:
         
         test_positions = visited
-        #print("Will test positions {}".format(visited))
+        print("original TP {}".format(test_positions))
         for test_pos in test_positions:
             i = 0
             test_direction = str(direction)
             test_start = copy.deepcopy(START)
-            test_visited = copy.deepcopy(visited)
+            test_visited = []
             test_vertical = copy.deepcopy(vertical)
             test_horizontal = copy.deepcopy(horizontal)
-
-            if test_direction == "N" or test_direction == "S":
-                test_vertical[test_pos[0]].append(test_pos[1])
-                test_vertical[test_pos[0]].sort()
-            if test_direction == "W" or test_direction == "E":
-                test_horizontal[test_pos[1]].append(test_pos[0])
-                test_horizontal[test_pos[1]].sort()
+            #V-- This cost me DAYS
+            #if test_direction == "N" or test_direction == "S":
+            test_vertical[test_pos[0]].append(test_pos[1])
+            test_vertical[test_pos[0]].sort()
+            #if test_direction == "E" or test_direction == "W":
+            test_horizontal[test_pos[1]].append(test_pos[0])
+            test_horizontal[test_pos[1]].sort()
             looped = False
             test_direction = str(DIRECTION)
-            #print("  Testing loop for {} facing {} from {}".format(test_pos, test_direction, test_start))
+            
             loop_test = 0
             loop_coords = []
             while True:
                 last_visited = copy.deepcopy(test_visited)
-
                 test_start, test_steps, test_visited, null = move(test_start, test_direction, test_horizontal, test_vertical, test_visited, False)
-                
+
                 if len(test_visited) == len(last_visited):
                     loop_test += 1
                     coord_string = "{}:{}:{}".format(test_start[0],test_start[1],test_direction)
-                    if coord_string == "96:122:E":
-                        print("moved {}".format(steps))
-                        print("     Testing loop block at {}".format(test_pos))
+                    short_coord_string = "{}:{}".format(test_pos[0],test_pos[1])
                     if len(loop_coords) == 24 and coord_string in loop_coords:
                         looped = True
                         print("  LOOP DETECTED with < 24 repetitions at test pos {} for coords {}".format(test_pos, coord_string))
                         print("    {}".format(loop_coords[-(loop_coords[::-1].index(coord_string)+1):]))
-                        break
+
                     if len(loop_coords) == 24:
                         loop_coords.pop(0)                      
                     loop_coords.append(coord_string)
                     if loop_test == 250:
                         looped = True
-                        print("  LONG LOOP DETECTED at {} for {}".format(test_pos, coord_string))
+                        if short_coord_string not in SAM:
+                            print("  LONG LOOP DETECTED at {} for {}".format(test_pos, coord_string))
                         break
                 elif -1 in test_start:
-                    #print("    test case exited map")
                     break
-                else:
-                    #Not at the end - and have new spaces to test  
-                    next
+                
                 test_direction = turn(test_direction)
-                #print("    TEST: at {} facing {}".format(test_start, test_direction))
                 i+= 1
+
             if looped is True:
                 if test_pos[0] not in horizontal[test_pos[1]]:
                     loop_positions.append("{}:{}".format(test_pos[0],test_pos[1]))
-                if coord_string == "96:122:E":
+                if short_coord_string not in SAM:
+                    print(last_visited)
                     print(loop_coords)
-                    print(coord_string)
-                #print(" ---- Block at postion {} loops".format(pos))
-
+                    for coord in loop_coords:
+                        x,y,d = coord.split(":")
+                        x = int(x)
+                        y = int(y)
+                        if d == "N":
+                            y = y + 1
+                        elif d == "S":
+                            y = y - 1
+                        elif d == "W":
+                            x = x - 1
+                        elif d == "E":
+                            x = x + 1
+                        print("Map Values in Loop:")
+                        print("{},{}".format(x,y))
+                        print((x in test_horizontal[y]) or (x == test_pos[0] and y == test_pos[1]))
+    
     return(pos, steps, all_visited, loop_positions)
 
-start, horizontal, vertical = get_data()
+start, horizontal, vertical, map = get_data()
 direction = "N"
 START = copy.deepcopy(start)
 DIRECTION = "N"
@@ -174,13 +202,13 @@ visited = []
 loops = []
 turns = 0
 while not done:
-    #print("moving from {} facing {}".format(start, direction))
     start, steps, visited, loop_positions = move(start, direction, horizontal, vertical, visited, True)
+
     Q1 += steps
     for object in loop_positions:
         if object not in loops:
             loops.append(object)
-    #print("  Moved {} steps".format(steps))
+
     if -1 in start:
         print("EXITED THE MAP")
         done = True
@@ -188,5 +216,5 @@ while not done:
     turns += 1
 
 print("Q1 Unique Steps: {}".format(len(visited)))
-print(loops)
+print(draw_map(map,visited))
 print("Q2: {}".format(len(loops)))
